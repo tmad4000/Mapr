775efcfbcd0235cb150a47e059cd3952d0e45260
Jacob Cole <tmad4000@gmail.com>
2013-05-28 02:39:46 -0400

    init

---
+++ home.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mapr -- Instantly make a Beautiful Map of Graduating Seniors' Locations for Future/Summer</title>
+    <base target="_blank">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+    <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-responsive.min.css" rel="stylesheet">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
+    <style>
+body {
+	padding-top: 60px; /* 60px to make the container go all the way to the bottom of the topbar */
+}
+.metric {
+	font-size: 10px;
+	color: #AAA;
+}
+.navbar-fixed-top .navbar-inner {
+	-webkit-box-shadow: 0 1px 3px rgba(0,0,0,.25), inset 0 -1px 0 rgba(0,0,0,.1) !important;
+	-moz-box-shadow: 0 1px 3px rgba(0,0,0,.25), inset 0 -1px 0 rgba(0,0,0,.1) !important;
+	box-shadow: 0 1px 3px rgba(0,0,0,.25), inset 0 -1px 0 rgba(0,0,0,.1) !important;
+}
+.progress {
+	height:80px;
+	margin-bottom:0;
+	width:828px;
+}
+.progress .bar {
+	padding-top:25px;
+	font-size:24px;
+}
+.progress-striped .bar {
+	-webkit-background-size: 80px 80px !important;
+	-moz-background-size: 80px 80px !important;
+	-o-background-size: 80px 80px !important;
+	background-size: 80px 80px !important;
+
+}
+</style>
+    </head>
+
+    <body>
+<div class="navbar navbar-inverse navbar-fixed-top">
+      <div class="navbar-inner">
+    <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+          <a class="brand" href="#" target="_self">Map<b>r</b></a>
+          <div class="nav-collapse collapse">
+        <ul class="nav">
+              <li class="active" target="_self"><a href="#">Home</a></li>
+              <li><a href="#about" target="_self">About</a></li>
+              <li><a href="#contact" target="_self">Contact</a></li>
+            </ul>
+      </div>
+          <!--/.nav-collapse --> 
+        </div>
+  </div>
+    </div>
+<div class="container">
+      <h1 dir="ltr" style="font-size:22px;line-height:normal"><a href="https://mapsengine.google.com/map/edit?mid=zQqqjq5RHCuE.k8hmxhWQVdzg">        Map of Harvard Eliot House Graduating Seniors 2013 <img src="120px-Icon_External_Link.svg.png" style="height:1em" /></a></h1>
+<div style="width:1070px;height:500px;overflow:hidden;position:relative;left:0px;top:0px">
+<iframe id="map-iframe" src="http://mapsengine.google.com/map/view?mid=zQqqjq5RHCuE.k8hmxhWQVdzg"  height="480" width="1670" style="position:absolute;left:-310px;top:-5px"></iframe>
+</div>
+
+      <br />
+      <div>
+    <p><span class="" style="font-size:22px;font-weight:bold">Percent Eliot 2013 seniors submitted to map. Add yourself at <a href="http://Eliot2013Seniors.tk" target="_blank">Eliot2013Seniors.tk</a>! </span>
+     
+    <a href="http://EliotProgressBars.tk" >
+    <div>
+        <div class="progress progress-striped ">
+      <div class="bar" style="width: 38%;">38%</div>
+    </div>
+    </div>
+        </a> <!--<span class="metric">Percent Eliot 2013 seniors submitted to map. Add yourself at <a href="http://Eliot2013Seniors.tk" target="_blank">Eliot2013Seniors.tk</a>! </span>--> &nbsp;
+    <br />
+Key Links:
+    <ul>
+          <li><a href="http://www.google.com/url?q=http%3A%2F%2FEliot2013Seniors.tk&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNGQR8AXsZJzBw7nbbZd3DAiIj76WA">Eliot2013Seniors.tk</a> - Add yourself! Goal: 100% of seniors add themselves by May 20th 2013<br />
+          </li>
+          <li> <a href="http://www.google.com/url?q=http%3A%2F%2FEliot2013Locations.tk&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFsVLHj878tBYdctnUyAk-drHW9vA">Eliot2013Locations.tk</a> - responses </li>
+          <li> <a href="http://www.google.com/url?q=http%3A%2F%2FEliotProgressBars.tk&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNHWM3CXcXP_5P-ZOLiZmdP3WY9-zg">EliotProgressBars.tk</a> - progress bar! ...and suggestion box.<br />
+          </li>
+          <li> <a href="http://www.google.com/url?q=http%3A%2F%2FEliot2013Map.tk&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNFvZJoFgx1LDdH4xabs7Q0mR-xJHw">Eliot2013Map.tk</a> - map of everybody! </li>
+        </ul>
+    </p>
+  </div>
+      <!--<script type="text/javascript">window.open('https://mapsengine.google.com/map/edit?mid=zQqqjq5RHCuE.k8hmxhWQVdzg');</script> -->
+    </div>
+</body>
+</html>
